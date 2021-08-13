25535ff7c616b3be502aa9881f86fdf5d087fad6
Pubudu Dananjaya <ppubudu4@gmail.com>
2021-08-13 10:31:33 +0530

    task add

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import taskReducer from './tackReducers';
 
-export default combineReducers({});
+export default combineReducers({
+  task: taskReducer,
+});
